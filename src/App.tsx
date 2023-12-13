fde87bf54b5d84656ccd89caa17d8b90e0dabc60
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-13 20:48:47 +0100

    left section setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { Milk } from './components/Milk.tsx'
 
 function App() {
-
   return (
     <div className="app">
-      <header>Welcome to cookie clicker by Mateusz Minder!</header>
-      <img src="/big-cookie.svg" />
+      <section className="section-left">
+        <header>Player's Bakery</header>
+        <div className="cookies-production-info">
+          <span>0 cookies</span>
+          <span>0 cookies per second</span>
+        </div>
+        <img className="big-cookie" src="/big-cookie.svg" />
+        <Milk color="white" />
+      </section>
+      <section className="section-middle">
+        <header>Welcome to cookie clicker by Mateusz Minder!</header>
+      </section>
+      <section className="section-right">
+        <header>Store</header>
+      </section>
     </div>
   )
 }
---
+++ src/components/Milk.tsx
@@ -0,0 +1,18 @@
+import './milk.css'
+
+type MilkProps = {
+  color: string,
+}
+
+export const Milk = ({ color }: MilkProps) => {
+  color
+
+  return (
+    <div className="milk">
+      <div className="wave wave1"></div>
+      <div className="wave wave2"></div>
+      <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
+    </div>
+  )
+}
