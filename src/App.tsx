f2c6715d1608394cb6419767e631a4a68cd8d9e9
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-13 22:21:22 +0100

    minor changes to big cookie + middle section header

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,25 @@ function App() {
           <span>0 cookies</span>
           <span>0 cookies per second</span>
         </div>
-        <img className="big-cookie" src="/big-cookie.svg" />
+        <div className="big-cookie-glow">
+          <img className="big-cookie" src="/big-cookie.svg" />
+        </div>
         <Milk color="white" />
       </section>
       <section className="section-middle">
-        <header>Welcome to cookie clicker by Mateusz Minder!</header>
+        <div className="header">
+          <section className="header-left header-section">
+            <button className="header-button">Options</button>
+            <button className="header-button">Stats</button>
+          </section>
+          <section className="header-middle header-section">
+            Welcome to Cookie Clicker clone by Mateusz Minder!
+          </section>
+          <section className="header-right header-section">
+            <button className="header-button">Info</button>
+            <button className="header-button">Restart</button>
+          </section>
+        </div>
       </section>
       <section className="section-right">
         <header>Store</header>
