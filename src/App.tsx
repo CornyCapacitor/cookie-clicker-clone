f15ea5c8d6a3f39d618c10dd4f041bdf08e523ac
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-13 19:31:23 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header>Welcome to cookie clicker by Mateusz Minder!</header>
+      <img src="/big-cookie.svg" />
+    </div>
+  )
+}
+
+export default App
