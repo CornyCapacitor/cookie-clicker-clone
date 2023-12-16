4182401dcd1ddd01c97cb562dc92e68f38085bbd
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-16 16:00:50 +0100

    cursor upgrades

--- src/gamedata/upgrades/cursorUpgrades.ts
+++ src/gamedata/upgrades/cursorUpgrades.ts
@@ -31,10 +31,10 @@ export const CursorUpgrades = [
     owned: false,
   },
   {
-    name: "",
-    description: "",
-    price: 0,
-    image: "",
+    name: "Ambidextrous",
+    description: "The mouse and cursors are twice as efficient.",
+    price: 10000,
+    image: "cursor/blueberyllium_cursor.webp",
     modifying: {
       building: "Cursor",
       other: "Clicks"
@@ -42,7 +42,187 @@ export const CursorUpgrades = [
     modifyingValue: 2.0,
     unlockCondition: {
       building: "Cursor",
-      amount: 1,
+      amount: 10,
+    },
+    owned: false,
+  },
+  {
+    name: "Thousand fingers",
+    description: "The mouse and cursors gain +0.1 cookies for each non-cursor object owned.",
+    price: 100000,
+    image: "cursor/chalcedhoney_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 0.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 25,
+    },
+    owned: false,
+  },
+  {
+    name: "Million fingers",
+    description: "Multiplies the gain from Thousand fingers by 5",
+    price: 10000000,
+    image: "cursor/buttergold_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 5.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 50,
+    },
+    owned: false,
+  },
+  {
+    name: "Billion fingers",
+    description: "Multiplies the gain from Thousand fingers by 10",
+    price: 100000000,
+    image: "cursor/sugarmuck_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 10.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 100,
+    },
+    owned: false,
+  },
+  {
+    name: "Trillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 100000000,
+    image: "cursor/jetmint_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 150,
+    },
+    owned: false,
+  },
+  {
+    name: "Quadrillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 1000000000,
+    image: "cursor/cherrysilver_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 200,
+    },
+    owned: false,
+  },
+  {
+    name: "Quintillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000,
+    image: "cursor/hazelrald_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 250,
+    },
+    owned: false,
+  },
+  {
+    name: "Sextillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000000000,
+    image: "cursor/mooncandy_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 300,
+    },
+    owned: false,
+  },
+  {
+    name: "Septillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000000000000,
+    image: "cursor/astrofudge_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 350,
+    },
+    owned: false,
+  },
+  {
+    name: "Octillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000000000000000,
+    image: "cursor/alabascream_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 400,
+    },
+    owned: false,
+  },
+  {
+    name: "Nonillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000000000000000000000,
+    image: "cursor/iridyum_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 450,
+    },
+    owned: false,
+  },
+  {
+    name: "Decillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000000000000000000000000,
+    image: "cursor/glucosmium_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 500,
+    },
+    owned: false,
+  },
+  {
+    name: "Undecillion fingers",
+    description: "Multiplies the gain from Thousand fingers by 20",
+    price: 10000000000000000000000000000000000,
+    image: "cursor/glimmerinque_cursor.webp",
+    modifying: {
+      other: "Thousand fingers",
+    },
+    modifyingValue: 20.0,
+    unlockCondition: {
+      building: "Cursor",
+      amount: 550,
     },
     owned: false,
   },
