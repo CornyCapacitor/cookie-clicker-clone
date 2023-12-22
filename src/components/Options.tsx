04c4005753f426ce9e87e1a16d295e8de837fcec
Mateusz Minder <mateusz.minder99@gmail.com>
2023-12-22 12:43:53 +0100

    header.css and middle-section css correction

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,9 +1,15 @@
 export const Options = () => {
   return (
-    <div className="options">
+    <div className="main">
       <header>Options</header>
-      <section className="options-section">
+      <section className="section">
         <header>General</header>
+        <section className="section-section">
+          <div>Volume:</div>
+          <input type="number" />
+          <div>Refresh rate:</div>
+          <input type="number" max="40" />
+        </section>
       </section>
       <span></span>
     </div>
